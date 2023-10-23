446a1f3db0b2e0bc6676cbde68a4677a56b34bb8
epolov <epolov@gmail.com>
2023-10-23 20:40:46 +0200

    Actualizar ejercicio 1 Introduccion JS

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,26 @@
+// Ejericio 1: Variables y Tipos de datos
+
+//1 declarar variable llamada "nombre"
+const nombre ='Eva'
+
+//2 declarar variable llamada "edad"
+const edad=37
+
+//3 declarar variable llamada "esEstudiante"
+const esEstudiante = true
+
+//4 declarar variable mensaje "mensaje"
+const mensaje = 'Hola mi nombre es ' + nombre + ' y tengo ' + edad + ' años. Soy estutudiante: ' + esEstudiante + '.'
+console.log(mensaje) // visualizar el mensaje
+// 5 variable num1
+const num1 = 5
+
+// 6 variable num2
+const num2 = 3
+
+// 7 HACER OPERACIONES
+
+const suma = num1 + num2 // Suma
+const resta = num1 - num2 // Resta
+const multiplica = num1 * num2 // multiplica
+const divide = num1 / num2
